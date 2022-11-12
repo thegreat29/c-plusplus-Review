041d0d449b3e00aee54c0b5c4cea78dde7ab32a1
thegreat29 <118030533+thegreat29@users.noreply.github.com>
2022-11-12 11:04:55 +0800

    if else and switch

--- review 1.1/review 1.1/review 1.1.cpp
+++ review 1.1/review 1.1/review 1.1.cpp
@@ -3,7 +3,8 @@ using namespace std;
 
 int main()
 {
-    float inp , a = 5;
+    //if else statment & new statement
+    /*float inp, a = 5; 
 
     cout << "Enter a number:";
     cin >> inp;
@@ -16,6 +17,48 @@ int main()
     }
     if (inp <= a ) {
         cout << "no it is less 2";
+
+    }*/
+
+    
+    int inp;
+
+    cout << "Enter a number day:";
+    cin >> inp;
+
+    switch (inp) {
+
+    case 1:
+        cout << "It's Sunday today";
+        break;
+
+    case 2:
+        cout << "It's Monday today";
+        break;
+
+    case 3:
+        cout << "It's Tuesday today";
+        break;
+
+    case 4:
+        cout << "It's Wednesday today";
+        break;
+
+    case 5:
+        cout << "It's Thursday today";
+        break;
+
+    case 6:
+        cout << "It's Friday today";
+        break;
+
+    case 7:
+        cout << "It's Saturday today";
+        break;
+
+    default :
+        cout << "Invalid input!";
+        break;
 
     }
 
