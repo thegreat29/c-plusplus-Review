1b10a51d64d2ecf33fb4df2dcd29b5b005590adc
thegreat29 <118030533+thegreat29@users.noreply.github.com>
2022-11-12 10:37:51 +0800

    If statement

--- review 1.1/review 1.1/review 1.1.cpp
+++ review 1.1/review 1.1/review 1.1.cpp
@@ -3,7 +3,21 @@ using namespace std;
 
 int main()
 {
-   
+    float inp , a = 5;
+
+    cout << "Enter a number:";
+    cin >> inp;
+
+    if (inp > a) {
+        cout << "yes it's greater ";
+    }else if (inp <= a){
+        cout << "no it is less";
+    
+    }
+    if (inp <= a ) {
+        cout << "no it is less 2";
+
+    }
 
 
     return 0;
